5449ebe2758b38e6028fde140dc452c74e00bda1
Ndahayo S Bertin <ndahayosibertin17@gmail.com>
2023-03-13 21:56:57 +0200

    Done with all CRUD Operation of the User

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const  mongoose = require('mongoose');
 const app = express()
 const Blog = require('./models/blogModel')
+const User = require('./models/userModel')
 app.use(express.json())
 
 //Updating the blog content using  the Form we have to use express urlencoded
@@ -85,6 +86,75 @@ app.delete('/blog_data/:id', async(req, res) => {
     }
 })
 
+
+/* Starting of the User Route Section */
+//Fetching User data from the database
+app.get('/user_data', async(req, res) =>{
+    try{
+        const user = await User.find({});
+        res.status(200).json(user)
+    }catch(error){
+        res.status(500).json({message: error.message})
+    }
+})
+
+//Fetching user data by id from the database
+app.get('/user_data/:id', async(req, res) =>{
+    try{
+        const {id} = req.params;
+        const user = await User.findById(id);
+        res.status(200).json(user)
+    }catch(error){
+        res.status(500).json({message: error.message})
+    }
+})
+
+//Saving user data into the database
+app.post('/user_data', async(req, res) => {
+    //console.log(req.body);
+    //res.send(req.body);
+    try{
+        const user = await User.create(req.body);
+        res.status(200).json(user);
+    }catch(error){
+            console.log(error.message);
+            res.status(500).json({message: error.message})
+        }
+})
+
+//Updating a user in database Using JSON
+app.put('/user_data/:id', async(req, res) =>{
+    try{
+        const {id} = req.params;
+        const user = await User.findByIdAndUpdate(id, req.body);
+        if(!user){
+            return res.status(404).json({message: `Cannot Find the User with this id: ${id}`})
+        }
+        const updatedUser = await User.findById(id);
+        res.status(200).json(updatedUser);
+        
+    }catch(error){
+        res.status(500).json({message: error.message})
+    }
+})
+
+//Deleting a user with specified ID
+app.delete('/user_data/:id', async(req, res) => {
+    try{
+        const {id} = req.params;
+        const user = await User.findByIdAndDelete(id);
+        if(!user){
+            return res.status(404).json({message: `Cannot find any a user with ID ${id}`})
+        }
+        res.status(200).json(user)
+    }catch(error){
+        res.status(500).json({message: error.message})
+    }
+})
+
+/* End of the User route section*/
+
+//Connecting to MongoDB 
 mongoose.set("strictQuery", false)
 mongoose.
 connect('mongodb+srv://Illustre:illustre123@mynodeapi.yls10be.mongodb.net/Blog-Node-API?retryWrites=true&w=majority')
